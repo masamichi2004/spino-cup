7e4ed188883a27beb10fc1fdfff4b61a3d6714e1
hiromuota166 <otahiromu166@gmail.com>
2024-09-25 02:32:08 +0900

    add: add training をexampleでmockとしてgetできるようになりました

---
+++ frontend/src/feature/AddMachoButton/AddMachoButton.tsx
@@ -0,0 +1,42 @@
+'use client';
+import React from "react";
+import { Button } from "../../../src/components/ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../../../src/components/ui/dialog";
+import { Input } from "../../../src/components/ui/input";
+import { Label } from "../../../src/components/ui/label";
+import { useCreateMacho } from './useCreateMacho';
+
+export function AddMachoButton() {
+  const { training, setTraining, handleClick } = useCreateMacho();
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild className="mb-2">
+        <Button variant="outline" className="w-full bg-green text-white">Add Training!</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>New Repository!</DialogTitle>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="weight" className="text-right">
+              Training Name
+            </Label>
+            <Input id="weight" type="string" onChange={(e) => setTraining(e.target.value)} value={training} className="col-span-3" />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button type="button" onClick={handleClick} className="bg-green">Create Repository</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
---
+++ frontend/src/feature/AddMachoButton/useCreateMacho.ts
@@ -0,0 +1,34 @@
+// hooks/useCreateMacho.ts
+import { useRouter } from "next/navigation";
+import { usePathname } from 'next/navigation';
+import { fetchPart } from './AddMacho';
+import { useState } from "react";
+
+export const useCreateMacho = () => {
+  const pathname = usePathname();
+  const segments = pathname.split('/').filter(Boolean);
+  const name = segments[1];
+  const part = segments[2];
+  const [training, setTraining] = useState<string>('');
+
+  const router = useRouter();
+
+  const handleClick = async () => {
+    if (name && training) {
+      try {
+        const result = await fetchPart(training);
+        if (result) {
+          router.push(`/home/${name}/${part}/${training}`);
+        } else {
+          console.error('Error: result is undefined or falsy');
+        }
+      } catch (error) {
+        console.error("Fetch error:", error);
+      }
+    } else {
+      console.error("name or part is undefined");
+    }
+  };
+
+  return { training, setTraining, handleClick };
+};
