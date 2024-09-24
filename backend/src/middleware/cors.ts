c95cee6bc93669d3c6aa5178d999d29eec606f35
MasamichiKanakubo <renlijinjiubao808@gmail.com>
2024-09-24 16:08:41 +0900

    fix: fix cors

--- backend/src/middleware/cors.ts
+++ backend/src/middleware/cors.ts
@@ -2,7 +2,7 @@ import { cors } from "hono/cors";
 
 export class CorsConfig {
   static readonly policy = cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://spino-cup.vercel.app/home"],
     allowHeaders: ["X-Custom-Header", "Upgrade-Insecure-Requests"],
     allowMethods: ["POST", "GET", "OPTIONS"],
     exposeHeaders: ["Content-Length", "X-Kuma-Revision"],
