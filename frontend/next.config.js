10cdfb361ad973aea30d33776ee7a244ce42baf4
hiromuota166 <otahiromu166@gmail.com>
2024-09-25 22:30:56 +0900

    fix : disable main

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,7 +5,7 @@ const withPWA = nextPWA({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
+  disable: process.env.NODE_ENV === "main",
 });
 
 const nextConfig = withPWA({
