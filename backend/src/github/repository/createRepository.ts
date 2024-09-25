d9ec60e0040578ab607ee718104b4ab1bd636866
shooooooma415 <shoma20040415@icloud.com>
2024-09-25 11:38:34 +0900

    add: add dir

--- backend/src/github/repository/createRepository.ts
+++ backend/src/github/repository/createRepository.ts
@@ -1,44 +1,61 @@
-import fetch from "node-fetch";
-import "dotenv/config";
-import { Repository } from "../../model/repository.model";
+import fetch from 'node-fetch';
+import 'dotenv/config';
+import { Repository } from "../../model/repository.model"
 
-const GITHUB_API_URL = "https://api.github.com";
+const GITHUB_API_URL = 'https://api.github.com';
+
+
+
 
 export class Repo {
-  private accessToken: string;
+    private accessToken: string;
 
-  constructor(accessToken: string) {
-    if (!accessToken) {
-      throw new Error("GitHub Access Token が設定されていません。");
+    constructor(accessToken: string) {
+        if (!accessToken) {
+            throw new Error('GitHub Access Token が設定されていません。');
+        }
+        this.accessToken = accessToken;
     }
-    this.accessToken = accessToken;
-  }
 
-  public async createRepo(repoName: string): Promise<Repository> {
-    const headers = {
-      Authorization: `Bearer ${this.accessToken}`,
+    public async createRepo(repoName: string): Promise<Repository> {
+        const headers = {
+            'Authorization': `Bearer ${this.accessToken}`,
+        };
+        const body = JSON.stringify({
+            name: repoName
+        });
+        const response = await fetch(`${GITHUB_API_URL}/user/repos`, {
+            method: 'POST',
+            headers: headers,
+            body: body,
+        });
+        const repoData = await response.json() as Repository;
+        return repoData;
     };
-    const body = JSON.stringify({
-      name: repoName,
-    });
-    const response = await fetch(`${GITHUB_API_URL}/user/repos`, {
-      method: "POST",
-      headers: headers,
-      body: body,
-    });
-    const repoData = (await response.json()) as Repository;
-    return repoData;
-  }
 
-  // public async createDir(
-  //     repo: string,
-  //     path: string,
-  //     commitMessage: string,
-  //     readmeContent: string
-  // ): Promise<void> {
-  //     const headers = {
-  //         'Authorization': `Bearer ${this.accessToken}`,
-  //     };
-
-  // }
+    public async createDir(
+        userId: string,
+        repo: string,
+        path: string,
+        commitMessage: string,
+        readmeContent: string
+    ): Promise<void> {
+        const headers = {
+            'Authorization': `Bearer ${this.accessToken}`,
+            'Content-Type': 'application/json'
+        };
+        const content = Buffer.from(readmeContent).toString('base64');
+        const body = JSON.stringify({
+            message: commitMessage,
+            content: content,
+        });
+        const response = await fetch(
+            `https://api.github.com/repos/${userId}/${repo}/contents/${path}/README.md`,
+            {
+                method: 'PUT',
+                headers: headers,
+                body: body
+            }
+        );
+    }
 }
