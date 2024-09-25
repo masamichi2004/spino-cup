0bd9a1a6f13410fc163029d7ef231651d635362d
hiromuota166 <otahiromu166@gmail.com>
2024-09-25 21:32:26 +0900

    add: 動画からコミット完成

--- frontend/src/feature/CommitDialogPage/CommitDialog.ts
+++ frontend/src/feature/CommitDialogPage/CommitDialog.ts
@@ -1,20 +1,39 @@
+import { getFromLocalStorage } from "../HomeNameMain/HomeName";
+
 export async function postWorkoutData(
   ownerId: string,
   repoName: string,
   dirName: string,
   sets: { weight: number; reps: number }[]
 ) {
-  const url = `http://localhost:8080/commit/${ownerId}/${repoName}/${dirName}`;
+  const url = `http://localhost:8080/commit`;
+  const timestamp = Date.now();
+
+  // localStorageからトークンを取得
+  const token = getFromLocalStorage('accessToken');
 
   const body = {
-    sets: sets
+    userId: ownerId,
+    repoName: repoName,
+    dirName: dirName,
+    jsonData: sets,
+    commitMessage: timestamp,
   };
+
+  // curlコマンド形式で出力
+  const curlCommand = `
+curl -X POST ${url} \\
+-H "Authorization: Bearer ${token}" \\
+-H "Content-Type: application/json" \\
+-d '${JSON.stringify(body, null, 2)}'
+  `;
+  console.log("Curl command to execute:\n", curlCommand);
 
   try {
     const response = await fetch(url, {
       method: 'POST',
       headers: {
-        // 後々localstorageからaccessTokenを取得するように変更する
+        'Authorization': `Bearer ${token}`,
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(body),
