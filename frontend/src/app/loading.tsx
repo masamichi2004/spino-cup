1cce408e0146f997c728684c1e736e0fc8019497
Taichi <takezohuto1123@icloud.com>
2024-09-26 11:10:16 +0900

    fix: component name

--- frontend/src/app/loading.tsx
+++ frontend/src/app/loading.tsx
@@ -1,5 +1,4 @@
 import { LoadingComp } from '@/src/feature/Loading/Loading';
-
 const Loading = () => {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-white">
