514644b75d6283f9b69e66a0ef9dbba083cb6e36
hiromuota166 <otahiromu166@gmail.com>
2024-09-26 09:52:18 +0900

    ハードコードを修正

--- frontend/src/feature/TrainingFile/TrainingFile.tsx
+++ frontend/src/feature/TrainingFile/TrainingFile.tsx
@@ -33,6 +33,7 @@ type FileItem = {
 
 export default function TrainingFile() {
   const router = useRouter();
+  const router = useRouter();
   const pathname = usePathname();
   const segments = pathname.split('/');
   const userId = segments[2];
@@ -42,7 +43,7 @@ export default function TrainingFile() {
   const [files, setFiles] = useState<FileItem[]>([]);
   const [token, setToken] = useState<string | null>(null);
   const [avatarUrl, setAvatarUrl] = useState<string | null>(null);
-
+  
   const handleSubmit = () => {
     <Loading />;
     if (dir) {
@@ -51,9 +52,9 @@ export default function TrainingFile() {
     setIsDialogOpen(false);
   };
 
-  const handleClick = (filename: string) => {
+  const handleClick = (filename : string) => {
     router.push(`/home/${userId}/${repoName}/${filename}`);
-  }
+  } 
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
